cd42a32b8516d62ba7ddbb1f70e6c44666b05478
Mateo Robayo <mapedorr@gmail.com>
2016-08-22 22:11:31 -0500

    Fixed a bug in the style of the 'detail-wrapper' for the Overlay directive. Improved the listener for scrolling. All browsers load new 3 cards when the scroll bar is in the bottom. Added a functionality that loads more cards based on the height of the content in the homepage and the height of the screen. 3 cards are loaded for devices with 'min-width' equals to 320 (smartphones).

--- assets/js/app/controllers/HomepageCtrl.js
+++ assets/js/app/controllers/HomepageCtrl.js
@@ -19,6 +19,13 @@
       // -----------------------------------------------------------------------
       // controller variables
       // -----------------------------------------------------------------------
+      var doc = $document[0];
+      var contentHeight = 0;
+      
+
+      // -----------------------------------------------------------------------
+      // scope properties
+      // -----------------------------------------------------------------------
       $scope.highlightedCard = null;
       $scope.selectedCard = null;
       $scope.cards = [];
@@ -32,25 +39,37 @@
        * the view.
        */
       $scope.initialize = function () {
-        // get all the art pieces in database
-        $gallerySrv.getAllArtPieces({skip: 0, limit: 3}, function (err, artPieces) {
-          if (err) return;
-          $scope.cards = artPieces;
-        });
+        var init = true;
+        var limit = ($window.screen.availHeight === 320 || $window.screen.availWidth === 320) ? 3 : 6;
+
+        // move the scroll bar to the top of the document (for Chrome and other explorers)
+        doc.documentElement.scrollTop = doc.body.scrollTop = 0;
 
         // add a listener for the scroll event in the browser
-        $document.on('scroll', function(e) {
-          var scrollY = e.target.defaultView.scrollY;
+        $window.addEventListener("scroll", function(e) {
+          if (init === true) {
+            // I beat you Firefox!
+            // Firefox starts listening the scroll event because it stores the
+            // scroll position in Y after refreshing the page, so this trick
+            // will force it to move the scroll bar to top in the very first event
+            // listening
+            doc.documentElement.scrollTop = doc.body.scrollTop = 0;
+            init = false;
+            return;
+          }
 
-          if (scrollY > 0 && scrollY >= e.target.defaultView.scrollMaxY) {
-            $gallerySrv.getAllArtPieces({skip: $scope.cards.length, limit: 3},
-              function (err, artPieces) {
-                if (err) return;
-                $scope.cards = $scope.cards.concat(artPieces);
-              }
-            );
+          // I took this operation from an old project, can't remember the source
+          var scrollPercentage = ((doc.documentElement.scrollTop + doc.body.scrollTop) / (doc.documentElement.scrollHeight - doc.documentElement.clientHeight) * 100);
+
+          // if the scroll is at its limit, load the next cluster of images
+          if (scrollPercentage >= 100) {
+            $scope.loadCards(3);
           }
         });
+
+        // get all the art pieces in database
+        $scope.loadCards(limit, $scope.checkIfMoreCardsNeeded);
+
       };
 
       /**
@@ -74,6 +93,51 @@
       $scope.showDetail = function (cardIndex) {
         $scope.selectedCard = cardIndex;
         $scope.cardsArray = $scope.cards;
+      };
+
+      /**
+       * Method that loads cards from the Gallery in the database and attach them
+       * to the current array of cards.
+       * 
+       * @param  {int}   _limit      The maximum amount of cards to load from  the
+       *                             database.
+       * @param  {Function} callback The function to call after completing the
+       *                             request.
+       */
+      $scope.loadCards = function (_limit, callback) {
+        $gallerySrv.getAllArtPieces({skip: $scope.cards.length, limit: _limit},
+          function (err, artPieces) {
+            if (err) return callback && (typeof callback === 'function') && callback(err);
+            $scope.cards = $scope.cards.concat(artPieces);
+            callback && (typeof callback === 'function') && callback();
+          }
+        );
+      };
+
+      /**
+       * Method that based on the height of the cards in the homepage and the
+       * header determines if more cards are needed to fill the view so the
+       * scroll bar is necessary. Other cards will be loaded during the scroll
+       * event.
+       * 
+       * @param  {Object} err An Object containing an error from a response error
+       *                      of the API.
+       */
+      $scope.checkIfMoreCardsNeeded = function (err) {
+        if (err) return;
+
+        // give angular 100 milliseconds to update the view, then check if more
+        // cards should be loaded
+        setTimeout(function () {
+          contentHeight = (doc.getElementById('main-header').clientHeight + doc.getElementById('homepage').clientHeight);
+
+          // if the height of the content of the page is lesser than the height
+          // of the whole page, load 3 more cards
+          if (contentHeight <= doc.documentElement.clientHeight) {
+            // load another 3 cards
+            $scope.loadCards(3, $scope.checkIfMoreCardsNeeded);
+          }
+        }, 100);
       };
 
       // -----------------------------------------------------------------------
